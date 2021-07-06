1d0b27e6441ba7b25a71a042f67a3333b8522d32
Gudipelli Sravya <69144046+19wh1a1255@users.noreply.github.com>
2021-07-06 10:31:53 +0530

    Create CharArraytoString.java

---
+++ JAVA Lab/cycle 1/Array programs/CharArraytoString.java
@@ -0,0 +1,9 @@
+class ChararraytoString
+{
+public static void main(String[] args)
+{
+char[] ch={'L', 'o', 'k', 'i',' ', 'o', 'd', 'i', 'n', 's', 'o', 'n'};
+String str=new String(ch);
+System.out.print("string: "+str);
+}
+}
